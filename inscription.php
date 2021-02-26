d113e75dd5389844fe72e4c5375bb4bad0745f0c
Edomiyas Temesgen <temesgen.edomiyas@yahoo.com>
2021-02-26 01:46:14 +0100

    Readme updated, styles finilased, database connection in progress

--- inscription.php
+++ inscription.php
@@ -15,15 +15,49 @@
         <form action="">
            <label for="pseudo">Pseudo</label>
            <input type="text" id="pseudo" name="pseudo"><br><br>
-           <label for="mot_de_passe">Mot de passe</label>
-           <input type="password" id="mot_de_passe" name="mot_de_passe"><br><br>
+           <label for="pass">Mot de passe</label>
+           <input type="password" id="pass" name="pass"><br><br>
            <label for="retapez_mp">Retapez votre mot de passe</label>
            <input type="password" id="retapez_mp" name="retapez_mp"><br><br> 
-           <label for="adresse_mail">Adresse email</label>
-           <input type="email" id="adresse_mail" name="adresse_mail"><br><br><br>
+           <label for="email">Adresse email</label>
+           <input type="email" id="email" name="email"><br><br><br>
            <input type="submit" id="button" value="S'inscrire">
         </form>
     </main>
+
+    <?php 
+    // Connexion à la base de données
+    try{
+        $bdd=new PDO('mysql:host=localhost;dbname=espace_membres;charset=utf8', 'root', '');
+        }
+        catch(Exception $e)
+        {
+            die('Erreur : '.$e->getMessage());
+        }
+    // Vérification de la validité des informations
+    if(isset($_POST['pseudo']) || isset($_POST['pass'])){
+        $email = $_POST["email"];
+       
+        if(!isset($_POST['pseudo']) || !isset($_POST['pass']) || !isset($_POST['email'])){
+            echo '<p>Vos données sont incomplètes !</p>';
+        }
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo 'Email invalide';
+        }
+        else{
+            // Hachage du mot de passe
+            $pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+            // Insertion
+            $req = $bdd->prepare('INSERT INTO membre(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
+            $req->execute(array(
+            'pseudo' => $pseudo,
+            'pass' => $pass_hache,
+            'email' => $email));
+            echo 'Inscription réussi';
+        }
+    }
+    ?>
     <footer>
         <?php echo "PHP & EDO - 2021";?>
     </footer>
